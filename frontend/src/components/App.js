0b4a2f397c9eb4cbba2230ffcfd9e21447738960
FeNjK <bodhisatva_xp@mail.ru>
2022-09-30 01:25:26 +0300

    fix: исправление ошибки авторизации на ВМ.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -20,6 +20,7 @@ function App() {
   const [currentUser, setCurrentUser] = useState({ name: "", about: "" });
   const [authorizationEmail, setAuthorizationEmail] = useState("");
   const [registration, setRegistration] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
@@ -27,7 +28,6 @@ function App() {
   const [cards, setCards] = useState([]);
   const [isConfirmPopupOpen, setIsConfirmPopupOpen] = useState(false);
   const [removeCard, setRemoveCard] = useState("");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [infoToolTipMessage, setInfoToolTipMessage] = useState(false);
 
   const history = useHistory();
