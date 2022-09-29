6a781f010509f3de20533eec30fd85a426e7e892
FeNjK <bodhisatva_xp@mail.ru>
2022-09-29 18:56:59 +0300

    fix: поменял порт для бекенда на 3000 чтобы пройти автотесты

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { NotFoundError } = require('./errors/http-status-codes');
 const { validURL } = require('./utils/regularExpressions');
 
-const { PORT = 4000 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
