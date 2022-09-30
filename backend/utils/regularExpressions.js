385ad02f350eb8082cb0070ed9de099cd9949e97
FeNjK <bodhisatva_xp@mail.ru>
2022-09-30 20:03:31 +0300

    fix: поменял регулярку для прохождения автотестов на URL

--- backend/utils/regularExpressions.js
+++ backend/utils/regularExpressions.js
@@ -2,7 +2,11 @@
 
 // для URL ()
 // eslint-disable-next-line no-useless-escape
-const validURL = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?$/mi;
+// const validURL = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?$/mi;
+
+// Для прохождения автотестов
+// eslint-disable-next-line no-useless-escape
+const validURL = /^(?:https?:\/\/)?[\w.-]+(?:\.[\w\.-]+)*[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.].+\*$/gmi;
 
 // для пароля
 const validPassword = /^[a-zA-Z0-9]{8,30}$/;
